78daa87385d4c430f05356efc50c0ce1d48ab7c5
Hùng Trần <tranphihung312@gmail.com>
2021-05-07 23:30:49 +0700

    create service and update app.component.ts

--- my-coin-app/src/app/app-routing.module.ts
+++ my-coin-app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { CreateTransactionComponent } from './pages/create-transaction/create-transaction.component';
+import { PendingTransactionsComponent } from './pages/pending-transactions/pending-transactions.component';
+import { WalletBalanceComponent } from './pages/wallet-balance/wallet-balance.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CreateTransactionComponent },
+  
+  {path: 'new/transaction', component: CreateTransactionComponent },
+  {path: 'new/transaction/pending', component: PendingTransactionsComponent },
+  {path: 'wallet/:address', component: WalletBalanceComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-coin-app/src/app/app.component.ts
+++ my-coin-app/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlockchainService } from './services/blockchain.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-coin-app';
+export class AppComponent implements OnInit {
+  public blockchain;
+  public showInfoMessage = true;
+  public publicKey;
+
+  constructor(private blockchainService: BlockchainService) {
+    this.blockchain = blockchainService.Instance;
+    this.publicKey = blockchainService.walletKeys[0].publicKey;
+  }
+
+  ngOnInit() {
+  }
+
+  thereArePendingTransactions() {
+    return this.blockchain.pendingTransactions.length > 0;
+  }
+
+  dismissInfoMessage() {
+    this.showInfoMessage = false;
+  }
 }
